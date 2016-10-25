21d04e3d212890bace57a3601caf12caefcf4587
Andrew Davis <andrew4d3@gmail.com>
2016-10-25 00:04:50 -0400

    example for named-exports

---
+++ chapter1/7-named-exports/logger.js
@@ -0,0 +1,10 @@
+// Page 41
+
+// info and verbose are gonna be public methods of this module
+exports.info = function (msg) {
+  console.log('info: ' + msg);
+}
+
+exports.verbose = function (msg) {
+  console.log('verbose: ' + msg);
+}
---
+++ chapter1/7-named-exports/main.js
@@ -0,0 +1,6 @@
+// Loading the module
+var logger = require('./logger');
+
+// Calling public methods from the module loaded
+logger.info("This is an info msg");
+logger.verbose("This is a verbose msg");
